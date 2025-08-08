dac61e0e8014a6054dd2f8187eed7b5ff48dd659
Xeno <1145764+MercuriusXeno@users.noreply.github.com>
2025-08-07 23:44:02 -0500

    Flailing about spawning altars and weird bugs

--- files/biomes/mountain/mountain_hall.lua
+++ files/biomes/mountain/mountain_hall.lua
@@ -1,4 +1,6 @@
 ---@diagnostic disable: lowercase-global, missing-global-doc
+RegisterSpawnFunction(0xff0a17a0, "spawn_target_altar")
+RegisterSpawnFunction(0xff6a17a9, "spawn_offer_altar")
 local old_init = init
 function init(x, y, w, h, ...)
   old_init(x, y, w, h, ...)
@@ -7,12 +9,12 @@ function init(x, y, w, h, ...)
   LoadPixelScene("mods/wand_workshop/files/biomes/mountain/hall.png",
     "mods/wand_workshop/files/biomes/mountain/hall_visual.png",
     scene_x, scene_y, "", true)
-  -- transforms have been standardized to a 58x58 that always has the same proportions/relative distances
-  local y_offset = -5
-  local target_altar_x = scene_x + 28
-  local target_altar_y = scene_y + y_offset
-  local offer_altar_x = target_altar_x
-  local offer_altar_y = target_altar_y + 48
-  EntityLoad("mods/wand_workshop/files/entities/target_altar.xml", target_altar_x, target_altar_y)
-  EntityLoad("mods/wand_workshop/files/entities/offer_altar.xml", offer_altar_x, offer_altar_y)
+end
+
+function spawn_target_altar(x, y)
+  EntityLoad("mods/wand_workshop/files/entities/target_altar.xml", x, y - 5)
+end
+
+function spawn_offer_altar(x, y)
+  EntityLoad("mods/wand_workshop/files/entities/offer_altar.xml", x, y - 5)
 end
--- files/biomes/temple/temple_altar_left.lua
+++ files/biomes/temple/temple_altar_left.lua
@@ -1,26 +1,22 @@
 ---@diagnostic disable: lowercase-global, missing-global-doc
-dofile_once("mods/wand_workshop/files/scripts/debug.lua")
+RegisterSpawnFunction(0xff0a17a0, "spawn_target_altar")
+RegisterSpawnFunction(0xff6a17a9, "spawn_offer_altar")
 local old_init = init
-Log("Overwriting old_init in temple_altar_left")
 function init(x, y, w, h, ...)
   old_init(x, y, w, h, ...)
   local scene_x = x + 314
-  -- even though the actual offset in the file is 86
-  -- the vanilla pixel scene has a +260 y offset. We apply that here.
   local scene_y = y + 346
-  LoadPixelScene(
-    "mods/wand_workshop/files/biomes/temple/altar_left.png",
+  LoadPixelScene("mods/wand_workshop/files/biomes/temple/altar_left.png",
     "mods/wand_workshop/files/biomes/temple/altar_left_visual.png",
-    scene_x, scene_y, "", true) -- needs the duplicate loader to work at all
-  -- transforms have been standardized to a 58x58 that always has the same proportions/relative distances
-  local y_offset = -5
-  local target_altar_x = scene_x + 28
-  local target_altar_y = scene_y + y_offset
-  local offer_altar_x = target_altar_x
-  local offer_altar_y = target_altar_y + 48
-  Log("Spawning target altar at " .. target_altar_x .. " " .. target_altar_y)
-  EntityLoad("mods/wand_workshop/files/entities/target_altar.xml", target_altar_x, target_altar_y)
+    scene_x, scene_y, "", true, false, {}, 100, false) -- fucking with every flag to restore sanity
+end
 
-  Log("Spawning offer altar at " .. offer_altar_x .. " " .. offer_altar_y)
-  EntityLoad("mods/wand_workshop/files/entities/offer_altar.xml", offer_altar_x, offer_altar_y)
+-- for reasons only god and the devil know the temple altar is off by one x AND y, despite
+-- being FUCKING IDENTICAL to the mountain_hall in every. EVERY. possible. way.
+function spawn_target_altar(x, y)
+  EntityLoad("mods/wand_workshop/files/entities/target_altar.xml", x + 1, y - 6)
+end
+
+function spawn_offer_altar(x, y)
+  EntityLoad("mods/wand_workshop/files/entities/offer_altar.xml", x + 1, y - 6)
 end
